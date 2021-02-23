a98e9084213b3c5386ea5d423ef9dd11cd1be8cf
Ross Lawley <ross.lawley@gmail.com>
2021-02-23 12:12:12 +0000

    Frontend

---
+++ frontend/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.div`
+  font-size: 24px;
+`;
+
+const App = () => (
+  <Header>Hello World!</Header>
+);
+
+export default App;
