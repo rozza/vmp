68cdbb389e65c81ed98bf5cba31753dd1694c40c
Maxime Beugnet <maxime.beugnet@gmail.com>
2021-02-24 23:12:56 +0100

    add automatic cluster scan of all dbs and collections.

--- backend/src/main/java/org/mongo/visualmongopro/graphql/CollectionDataFetcher.java
+++ backend/src/main/java/org/mongo/visualmongopro/graphql/CollectionDataFetcher.java
@@ -22,7 +22,10 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
 import graphql.schema.DataFetcher;
+import org.bson.BsonArray;
+import org.bson.BsonDocument;
 import org.bson.BsonType;
+import org.bson.BsonValue;
 import org.bson.Document;
 import org.bson.codecs.BsonTypeClassMap;
 import org.bson.codecs.DocumentCodecProvider;
@@ -36,17 +39,23 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.empty;
+import static com.mongodb.client.model.Filters.eq;
 import static java.util.Arrays.asList;
 
 @Component
 public class CollectionDataFetcher {
   private final MongoCollection<Document> collectionMetadata;
+  private final MongoClient client;
 
   public CollectionDataFetcher(@Autowired MongoClient client) {
+    this.client = client;
     MongoDatabase visualMongoDBProDatabase = client.getDatabase("visualmongodbpro");
     this.collectionMetadata =
         visualMongoDBProDatabase
@@ -61,6 +70,32 @@ public class CollectionDataFetcher {
                     visualMongoDBProDatabase.getCodecRegistry()));
   }
 
+  private static Document cleanType(String type) {
+    Document typeDoc = new Document("bson_type", type);
+    String formType = type;
+    switch (type) {
+      case "date_time" -> {
+        formType = "string";
+        typeDoc.append("format", "date-time");
+      }
+      case "int32" -> formType = "integer";
+      case "double", "decimal128" -> formType = "number";
+      case "object_id" -> formType = "string";
+    }
+    return typeDoc.append("type", formType);
+  }
+
+  private static String cleanKey(String key) {
+    String s = key.replaceAll("[_-]", " ");
+    s = Arrays.stream(s.split("(?=[A-Z])"))
+        .map(st -> st.substring(0, 1).toUpperCase() + st.substring(1))
+        .collect(Collectors.joining(" "));
+    s = Arrays.stream(s.trim().split(" "))
+        .map(st -> st.substring(0, 1).toUpperCase() + st.substring(1))
+        .collect(Collectors.joining(" "));
+    return s.equals("Id") ? "ID" : s;
+  }
+
   @PostConstruct
   public void init() {
     collectionMetadata.drop();
@@ -70,64 +105,119 @@ public class CollectionDataFetcher {
                 "databaseName", "test",
                 "collectionName", "test1",
                 "jsonSchema", Document.parse("{" +
-                                             "  \"title\": \"A registration form\"," +
-                                             "  \"description\": \"A simple form example.\"," +
-                                             "  \"type\": \"object\"," +
-                                             "  \"required\": [" +
-                                             "    \"firstName\"," +
-                                             "    \"lastName\"" +
-                                             "  ]," +
-                                             "  \"properties\": {" +
-                                             "    \"firstName\": {" +
-                                             "      \"type\": \"string\"," +
-                                             "      \"title\": \"First name\"," +
-                                             "      \"default\": \"Chuck\"" +
-                                             "    }," +
-                                             "    \"lastName\": {" +
-                                             "      \"type\": \"string\"," +
-                                             "      \"title\": \"Last name\"" +
-                                             "    }," +
-                                             "    \"telephone\": {" +
-                                             "      \"type\": \"string\"," +
-                                             "      \"title\": \"Telephone\"," +
-                                             "      \"minLength\": 10" +
-                                             "    }" +
-                                             "  }" +
-                                             "}"),
+                    "  \"title\": \"A registration form\"," +
+                    "  \"description\": \"A simple form example.\"," +
+                    "  \"type\": \"object\"," +
+                    "  \"required\": [" +
+                    "    \"firstName\"," +
+                    "    \"lastName\"" +
+                    "  ]," +
+                    "  \"properties\": {" +
+                    "    \"firstName\": {" +
+                    "      \"type\": \"string\"," +
+                    "      \"title\": \"First name\"," +
+                    "      \"default\": \"Chuck\"" +
+                    "    }," +
+                    "    \"lastName\": {" +
+                    "      \"type\": \"string\"," +
+                    "      \"title\": \"Last name\"" +
+                    "    }," +
+                    "    \"telephone\": {" +
+                    "      \"type\": \"string\"," +
+                    "      \"title\": \"Telephone\"," +
+                    "      \"minLength\": 10" +
+                    "    }" +
+                    "  }" +
+                    "}"),
                 "isSchemaAutoGenerated", false,
                 "uiSchema", Document.parse("{" +
-                                           "  \"firstName\": {" +
-                                           "    \"ui:autofocus\": true," +
-                                           "    \"ui:emptyValue\": \"\"," +
-                                           "    \"ui:autocomplete\": \"family-name\"" +
-                                           "  }," +
-                                           "  \"lastName\": {" +
-                                           "    \"ui:emptyValue\": \"\"," +
-                                           "    \"ui:autocomplete\": \"given-name\"" +
-                                           "  }," +
-                                           "  \"age\": {" +
-                                           "    \"ui:widget\": \"updown\"," +
-                                           "    \"ui:title\": \"Age of person\"," +
-                                           "    \"ui:description\": \"(earthian year)\"" +
-                                           "  }," +
-                                           "  \"bio\": {" +
-                                           "    \"ui:widget\": \"textarea\"" +
-                                           "  }," +
-                                           "  \"password\": {" +
-                                           "    \"ui:widget\": \"password\"," +
-                                           "    \"ui:help\": \"Hint: Make it strong!\"" +
-                                           "  }," +
-                                           "  \"date\": {" +
-                                           "    \"ui:widget\": \"alt-datetime\"" +
-                                           "  }," +
-                                           "  \"telephone\": {" +
-                                           "    \"ui:options\": {" +
-                                           "      \"inputType\": \"tel\"" +
-                                           "    }" +
-                                           "  }" +
-                                           "}")
+                    "  \"firstName\": {" +
+                    "    \"ui:autofocus\": true," +
+                    "    \"ui:emptyValue\": \"\"," +
+                    "    \"ui:autocomplete\": \"family-name\"" +
+                    "  }," +
+                    "  \"lastName\": {" +
+                    "    \"ui:emptyValue\": \"\"," +
+                    "    \"ui:autocomplete\": \"given-name\"" +
+                    "  }," +
+                    "  \"age\": {" +
+                    "    \"ui:widget\": \"updown\"," +
+                    "    \"ui:title\": \"Age of person\"," +
+                    "    \"ui:description\": \"(earthian year)\"" +
+                    "  }," +
+                    "  \"bio\": {" +
+                    "    \"ui:widget\": \"textarea\"" +
+                    "  }," +
+                    "  \"password\": {" +
+                    "    \"ui:widget\": \"password\"," +
+                    "    \"ui:help\": \"Hint: Make it strong!\"" +
+                    "  }," +
+                    "  \"date\": {" +
+                    "    \"ui:widget\": \"alt-datetime\"" +
+                    "  }," +
+                    "  \"telephone\": {" +
+                    "    \"ui:options\": {" +
+                    "      \"inputType\": \"tel\"" +
+                    "    }" +
+                    "  }" +
+                    "}")
             ))
         ));
+    collectionMetadata.insertMany(scanCollectionMetadata(client));
+  }
+
+  private List<Document> scanCollectionMetadata(MongoClient client) {
+    List<Document> databases = client.listDatabases().into(new ArrayList<>());
+    List<String> dbs = databases.stream().filter(db -> {
+      List<String> admin_dbs = Arrays.asList("admin", "config", "local");
+      return !admin_dbs.contains(db.getString("name"));
+    }).map(doc -> doc.getString("name")).collect(Collectors.toList());
+
+    List<Document> metadata = new ArrayList<>();
+
+    for (String dbString : dbs) {
+      MongoDatabase db = client.getDatabase(dbString);
+
+      for (String coll : db.listCollectionNames()) {
+        BsonDocument doc = db.getCollection(coll, BsonDocument.class).find().first();
+        Document metadataDoc = new Document("databaseName", dbString).append("collectionName", coll).append("isSchemaAutoGenerated", true);
+        metadataDoc.append("jsonSchema", extractJsonSchemaFromDoc(doc));
+        metadata.add(metadataDoc);
+      }
+    }
+    return metadata;
+  }
+
+  private Document extractJsonSchemaFromDoc(BsonDocument doc) {
+    Document propsContent = new Document();
+    Document schema = new Document("type", "object").append("properties", propsContent);
+
+    for (Map.Entry<String, BsonValue> entry : doc.entrySet()) {
+      String key = entry.getKey();
+      BsonType bsonType = entry.getValue().getBsonType();
+      if (!bsonType.isContainer()) {
+        Document content = cleanType(bsonType.toString().toLowerCase());
+        content.append("title", cleanKey(key));
+        propsContent.append(key, content);
+      } else if (bsonType.equals(BsonType.ARRAY)) {
+        BsonArray array = entry.getValue().asArray();
+        if (array.size() > 0) { // todo ignoring empty arrays...
+          BsonValue first = array.get(0);
+          if (first.getBsonType().equals(BsonType.DOCUMENT)) {
+            Document content = cleanType(bsonType.toString().toLowerCase());
+            content.append("title", cleanKey(key)).append("items", extractJsonSchemaFromDoc(first.asDocument()));
+            propsContent.append(key, content);
+          } else {
+            Document content = cleanType(bsonType.toString().toLowerCase());
+            content.append("title", cleanKey(key)).append("items", cleanType(first.getBsonType().toString().toLowerCase()));
+            propsContent.append(key, content);
+          }
+        }
+      } else if (bsonType.equals(BsonType.DOCUMENT)) {
+        propsContent.append(key, extractJsonSchemaFromDoc(entry.getValue().asDocument()).append("title", cleanKey(key)));
+      }
+    }
+    return schema;
   }
 
   public DataFetcher<List<Map<String, Object>>> getCollectionsDataFetcher() {
